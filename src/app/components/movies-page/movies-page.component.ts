f7b33a0ef4cb3f200a960a486e25792087e13378
Darya-Slugina <74356734+Darya-Slugina@users.noreply.github.com>
2021-06-17 14:51:29 +0300

    Movies (#3)
    * adding header and basic routing
    
    * http request for all movies and movie service
    
    * fixing header style
    
    * structure changes
    
    * delete all-movies component
    
    Co-authored-by: Marina Damyanova <marina_damyanova@epam.com>
    Co-authored-by: Viкtor <vkaraulanov@gmail.com>

---
+++ src/app/components/movies-page/movies-page.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Movie } from 'src/app/shared/models/movie.model';
+import { MoviesService } from 'src/app/shared/services/movies.service';
+
+@Component({
+  selector: 'app-movies-page',
+  templateUrl: './movies-page.component.html',
+  styleUrls: ['./movies-page.component.css']
+})
+export class MoviesPageComponent implements OnInit {
+  movies: Movie[] = [];
+  subscription: Subscription;
+
+  constructor(private movieService: MoviesService) {}
+
+  ngOnInit() {
+    this.movieService.getMovies().subscribe();
+    this.movieService.moviesChanged.subscribe((res) => {
+      this.movies = res;
+      console.log(this.movies);
+    });
+  }
+}
