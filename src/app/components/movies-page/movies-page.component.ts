8f43db9e1de78c51b82534596b0d409a7ce7643d
Darya Slugina <d.slugina@gmail.com>
2021-06-17 14:49:09 +0300

    delete all-movies component

--- src/app/components/movies-page/movies-page.component.ts
+++ src/app/components/movies-page/movies-page.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Movie } from 'src/app/shared/models/movie.model';
+import { MoviesService } from 'src/app/shared/services/movies.service';
 
 @Component({
   selector: 'app-movies-page',
@@ -6,10 +9,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./movies-page.component.css']
 })
 export class MoviesPageComponent implements OnInit {
+  movies: Movie[] = [];
+  subscription: Subscription;
 
-  constructor() { }
+  constructor(private movieService: MoviesService) {}
 
   ngOnInit() {
+    this.movieService.getMovies().subscribe();
+    this.movieService.moviesChanged.subscribe((res) => {
+      this.movies = res;
+      console.log(this.movies);
+    });
   }
-
 }
