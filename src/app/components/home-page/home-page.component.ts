62cedc02631fa64a67cdfedbced54774ca66ba42
Darya Slugina <d.slugina@gmail.com>
2021-06-23 14:25:28 +0300

    limk to all directors on home page

--- src/app/components/home-page/home-page.component.ts
+++ src/app/components/home-page/home-page.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Movie } from 'src/app/shared/models/movie.model';
+import { MoviesService } from 'src/app/shared/services/movies.service';
 
 @Component({
   selector: 'app-home-page',
@@ -6,5 +8,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./home-page.component.css']
 })
 export class HomePageComponent{
+  movies: any;
+
+  constructor(
+    private moviesService: MoviesService
+  ) {}
+
+  ngOnInit(): void {
+    this.movies = this.moviesService.getMovies();
+
+    const moviesArr: Movie[] = [];
+    for (let key in this.movies) {
+      if (this.movies.hasOwnProperty(key)) {
+        moviesArr.push(this.movies[key]);
+      }
+    }
+    this.movies = moviesArr;
+  }
   
 }
