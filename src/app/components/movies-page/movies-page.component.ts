62ffcbaabdfb4884822809d70b73904d3ca16a89
Darya Slugina <d.slugina@gmail.com>
2021-06-23 13:44:34 +0300

    toggle view

--- src/app/components/movies-page/movies-page.component.ts
+++ src/app/components/movies-page/movies-page.component.ts
@@ -1,39 +1,83 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Observable, Subscription } from 'rxjs';
+import { Subscription } from 'rxjs';
+import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { Movie } from 'src/app/shared/models/movie.model';
 import { DataStorageService } from 'src/app/shared/services/data-store.service';
 import { MoviesService } from 'src/app/shared/services/movies.service';
 
 @Component({
-  selector: 'app-movies-page',
+  selector: 'app-movie',
   templateUrl: './movies-page.component.html',
   styleUrls: ['./movies-page.component.css'],
 })
 export class MoviesPageComponent implements OnInit {
   movies: any;
-  private subscription: Subscription;
-  constructor(private movieService: MoviesService) {}
+  subscription: Subscription;
+  isTableView: boolean = true;
 
-  ngOnInit() {
-    // this.movies = this.movieService.getMovies();
-    // this.movieService.moviesChanged.subscribe(
-    //   (movies: any) => {
-    //     const moviesArr: Movie[] = [];
-    //     for (let key in movies) {
-    //       if (movies.hasOwnProperty(key)) {
-    //         moviesArr.push(movies[key]);
-    //       }
-    //     }
-    //     console.log(moviesArr);
-    //     this.movies = moviesArr;
-    //   }
-    // );
+  constructor(
+    private moviesService: MoviesService,
+    private dataStorageService: DataStorageService
+  ) {}
+
+  ngOnInit(): void {
+    this.movies = this.moviesService.getMovies();
+    console.log(this.movies);
+    this.getMoviesArr();
+   this.moviesService.moviesChanged.subscribe((movies: any) => {
+      this.movies = movies;
+    });
+    console.log(this.movies);
+    console.log(this.isTableView);
   }
 
-  // getMovies(): Observable<any> {
-  //   return this.movieService.moviesChanged;
-  // }
-  // ngOnDestroy() {
-  //   this.subscription.unsubscribe();
-  // }
+  getMoviesArr() {
+    const moviesArr: Movie[] = [];
+    for (let key in this.movies) {
+      if (this.movies.hasOwnProperty(key)) {
+        moviesArr.push(this.movies[key]);
+      }
+    }
+    this.movies = moviesArr;
+  }
+
+  ascendingSort(prop: string) {
+    if (prop === 'year') {
+      this.movies.sort((a, b) =>
+        a[prop].slice(a[prop].lastIndexOf('-')) >
+        b[prop].slice(b[prop].lastIndexOf('-'))
+          ? -1
+          : 1
+      );
+    } else {
+      this.movies.sort((a, b) =>
+        a[prop].toLowerCase() > b[prop].toLowerCase() ? -1 : 1
+      );
+    }
+  }
+
+  descendingSort(prop: string) {
+    if (prop === 'year') {
+      this.movies.sort((a, b) =>
+        a[prop].slice(a[prop].lastIndexOf('-')) >
+        b[prop].slice(b[prop].lastIndexOf('-'))
+          ? 1
+          : -1
+      );
+    } else {
+      this.movies.sort((a, b) =>
+        a[prop].toLowerCase() > b[prop].toLowerCase() ? 1 : -1
+      );
+    }
+  }
+
+  onDelete(title) {
+    this.moviesService.deleteMovie(title);
+    this.dataStorageService.deleteMovie(title);
+    this.getMoviesArr();
+  }
+
+  onChangeView() {
+    this.isTableView = !this.isTableView;
+    console.log(this.isTableView);
+  }
 }
