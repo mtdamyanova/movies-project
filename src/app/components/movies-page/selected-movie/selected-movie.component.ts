4eefac586939d0145e31986707565a4cc831175f
Viktor Karaulanov <viktor_karaulanov@epam.com>
2021-06-29 10:59:00 +0300

    getting selected movie through moviesArray (works on refreshing the page)

--- src/app/components/movies-page/selected-movie/selected-movie.component.ts
+++ src/app/components/movies-page/selected-movie/selected-movie.component.ts
@@ -1,22 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { interval } from 'rxjs';
+import { map, switchMap, tap } from 'rxjs/operators';
 import { Movie } from 'src/app/shared/models/movie.model';
 import { MoviesService } from 'src/app/shared/services/movies.service';
 
 @Component({
   selector: 'app-selected-movie',
   templateUrl: './selected-movie.component.html',
-  styleUrls: ['./selected-movie.component.css']
+  styleUrls: ['./selected-movie.component.css'],
 })
 export class SelectedMovieComponent implements OnInit {
   movie: Movie;
   title: string;
   hoverDescription: boolean = false;
-  constructor(private moviesService: MoviesService, private route: ActivatedRoute) { }
+  constructor(
+    public moviesService: MoviesService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit() {
     this.title = this.route.snapshot.paramMap.get('title');
-    this.movie = this.moviesService.getMovie(this.title);
+    // this.movie = this.moviesService.getMovie(this.title);
+    this.moviesService.moviesArray.subscribe((movies) => {
+      this.movie = movies.filter((data) => data.title == this.title)[0];
+    });
   }
 
   onMouseOver() {
--- src/app/shared/services/movies.service.ts
+++ src/app/shared/services/movies.service.ts
@@ -14,16 +14,23 @@ export class MoviesService {
   public readonly moviesArray = this._movies$
     .asObservable()
     .pipe(map((movies) => this.getMoviesAsArray(movies)));
+  private isLoading$ = new BehaviorSubject<boolean>(false);
+  public readonly isLoading = this.isLoading$.asObservable();
 
   constructor(private apiService: DataStorageService) {
     this.getMovies();
   }
 
   public getMovies() {
-    this.apiService.fetchMovies().subscribe((data) => {
-      this._movies$.next(data);
-      console.log(this._movies$, 'subject-a');
-    });
+    this.isLoading$.next(true);
+    this.apiService
+      .fetchMovies()
+      .pipe(tap(() => this.isLoading$.next(false)))
+      .subscribe((data) => {
+        this._movies$.next(data);
+        this.isLoading$.next(false);
+        console.log(this.isLoading$.getValue(), 11111);
+      });
   }
 
   public addMovie(movie: Movie): void {
